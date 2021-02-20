b24981067c5b48cc6cb24672e0d80f4e6ff3e403
Mutaz Fattal <mzfattal@hotmail.com>
2021-02-20 18:37:23 -0500

    initial setup

---
+++ frontend/src/App.tsx
@@ -0,0 +1,71 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+
+const useStyles = makeStyles({
+  root: {
+    backgroundColor: "black",
+    color: "white",
+  },
+});
+
+export default function App() {
+  const classes = useStyles();
+
+  return (
+    <Router>
+      <AppBar position="static">
+        <Toolbar>
+          {/* <IconButton
+            edge="start"
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="menu"
+          >
+            <MenuIcon />
+          </IconButton> */}
+          <Link to="/">
+            <Button variant="text">Home</Button>
+          </Link>
+          <Link to="/about">
+            <Button variant="text">about</Button>
+          </Link>
+          <Link to="/users">
+            <Button variant="text">users</Button>
+          </Link>
+        </Toolbar>
+      </AppBar>
+      <div className={classes.root}>
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/users">
+            <Users />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function Users() {
+  return <h2>Users</h2>;
+}
