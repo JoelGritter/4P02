48d707689efb00c51f0d4a57e17a4032048647c5
Mutaz Fattal <mzfattal@hotmail.com>
2021-02-25 09:41:34 -0500

    material ui theme setup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,38 +7,50 @@ import CoursesPage from "./pages/CoursesPage";
 import HomePage from "./pages/HomePage";
 import ProfilePage from "./pages/ProfilePage";
 import TestsPage from "./pages/TestsPage";
+import { ThemeProvider, createMuiTheme } from "@material-ui/core/styles";
+import { Paper } from "@material-ui/core";
 
 export default function App() {
+  const theme = createMuiTheme({
+    palette: {
+      type: "dark",
+    },
+  });
+
   return (
-    <Router>
-      <div>
-        <Switch>
-          <Route path="/login">
-            <LoginPage />
-          </Route>
-          <Route path="/addCourse">
-            <AddCoursePage />
-          </Route>
-          <Route path="/assignments">
-            <AssignmentsPage />
-          </Route>
-          <Route path="/courses">
-            <CoursesPage />
-          </Route>
-          <Route path="/profile">
-            <ProfilePage />
-          </Route>
-          <Route path="/tests">
-            <TestsPage />
-          </Route>
-          <Route path="/home">
-            <HomePage />
-          </Route>
-          <Route path="/">
-            <HomePage />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
+    <ThemeProvider theme={theme}>
+      <Paper style={{ height: "100vh" }}>
+        <Router>
+          <div>
+            <Switch>
+              <Route path="/login">
+                <LoginPage />
+              </Route>
+              <Route path="/addCourse">
+                <AddCoursePage />
+              </Route>
+              <Route path="/assignments">
+                <AssignmentsPage />
+              </Route>
+              <Route path="/courses">
+                <CoursesPage />
+              </Route>
+              <Route path="/profile">
+                <ProfilePage />
+              </Route>
+              <Route path="/tests">
+                <TestsPage />
+              </Route>
+              <Route path="/home">
+                <HomePage />
+              </Route>
+              <Route path="/">
+                <HomePage />
+              </Route>
+            </Switch>
+          </div>
+        </Router>
+      </Paper>
+    </ThemeProvider>
   );
 }
