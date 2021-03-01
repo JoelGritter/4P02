5106e98dcfd517cca7d43b0d04111e58399df9e1
kindeep <kindeepsingh@gmail.com>
2021-03-01 11:10:44 -0500

    Remove console.log

--- lambda/app/util/env.ts
+++ lambda/app/util/env.ts
@@ -4,6 +4,4 @@ dotenv.config({
   path: ".env",
 });
 
-console.log(process.env.USER_POOL_ID, process.env.USER_POOL_CLIENT_ID);
-
 export const {DB, USER_POOL_ID, USER_POOL_CLIENT_ID} = process.env;
