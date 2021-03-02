4a9541704ecf21319ec11b23a17548c81428abe6
Kindeep Singh Kargil <kindeepsingh@gmail.com>
2021-03-01 20:05:22 -0500

    move cognito stuff to env file

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -18,10 +18,10 @@ import { AmplifyAuthenticator, AmplifySignUp } from "@aws-amplify/ui-react";
 import { SnackbarProvider } from "notistack";
 
 const awsConfig = {
-  aws_project_region: "us-east-1",
-  aws_cognito_region: "us-east-1",
-  aws_user_pools_id: "us-east-1_rxv5ynpIW",
-  aws_user_pools_web_client_id: "1etk2iv3214e73cfeiveinsfkn",
+  aws_project_region: process.env.REACT_APP_AWS_PROJECT_REGION,
+  aws_cognito_region: process.env.REACT_APP_AWS_COGNITO_REGION,
+  aws_user_pools_id: process.env.REACT_APP_AWS_USER_POOLS_ID,
+  aws_user_pools_web_client_id: process.env.REACT_APP_AWS_USER_POOLS_WEB_CLIENT_ID,
 };
 
 Amplify.configure(awsConfig);
