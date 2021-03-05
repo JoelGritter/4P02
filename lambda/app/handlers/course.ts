8d7435955ab134664e0dccbcfbfe5e36dce21168
JoelGritter <joel@gritter.ca>
2021-03-05 17:19:28 -0500

    Update permissions + checks
    - automatically add professor to course if they are making the course
    - prevent professors from removing themselves from course
    - prevent professors from accessing courses they are not designated to

--- lambda/app/handlers/course.ts
+++ lambda/app/handlers/course.ts
@@ -10,8 +10,17 @@ export const getAll = lambda(roleAuth(['admin'], async (event) => {
 	return success(courses);
 }));
 
-export const addCourse = lambda(roleAuth(['admin', 'prof'], async (event) => {
+export const addCourse = lambda(roleAuth(['admin', 'prof'], async (event, context, { userDoc }) => {
 	const newCourse = parseBody<Course>(event);
+
+	const reqUser = userDoc as User;
+	const { cognitoId } = reqUser;
+
+	// add current user to this new course if user is prof
+	if(reqUser.roles.includes('prof')){
+		newCourse.currentProfessors.push(cognitoId);
+	}
+
 	const resCourse = await CourseModel.create(newCourse);
 	return success(resCourse);
 }));
@@ -25,6 +34,11 @@ export const updateCourse = lambda(roleAuth(['admin', 'prof'], async (event, con
 
 	if(reqUser.roles.includes('admin') || resCourse.currentProfessors.includes(cognitoId)){
 		if(newCourse._id) delete newCourse._id;
+
+		// prevent professor from removing themselves from course
+		if(!newCourse.currentProfessors.includes(cognitoId)){
+			newCourse.currentProfessors.push(cognitoId);
+		}
 
 		const updatedCourse = await CourseModel.findByIdAndUpdate(event.pathParameters.id, newCourse, {
 			new: true,
@@ -40,7 +54,15 @@ export const deleteCourse = lambda(roleAuth(['admin'], async (event) => {
 	return badRequest("Unsupported: Cannot delete course");
 }));
 
-export const getCourse = lambda(roleAuth(['admin', 'prof'], async (event) => {
+export const getCourse = lambda(roleAuth(['admin', 'prof'], async (event, context, { userDoc }) => {
 	const resCourse = await CourseModel.findById(event.pathParameters.id);
-	return success(resCourse);
+
+	const reqUser = userDoc as User;
+	const { cognitoId } = reqUser;
+
+	if(reqUser.roles.includes('admin') || resCourse.currentProfessors.includes(cognitoId)){
+		return success(resCourse);
+	} else {
+		return unauthorized("Insufficient Privileges: Cannot access course");
+	}
 }));
