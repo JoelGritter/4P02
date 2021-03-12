ae40086522d3d777c1d1227f892597bcf9e871e6
Mutaz Fattal <mzfattal@hotmail.com>
2021-03-12 00:48:27 -0500

    completed initial create course page setup

--- frontend/src/pages/instructorPages/CreateCoursePage.tsx
+++ frontend/src/pages/instructorPages/CreateCoursePage.tsx
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import { makeStyles, Theme } from "@material-ui/core/styles";
 import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
+import { useSnackbar } from "notistack";
+import { CreateCourseForm } from "../../components/CreateCourseForm";
+import { post } from "../../api/util";
 
 const useStyles = makeStyles((theme: Theme) => ({
   root: {},
@@ -18,7 +21,45 @@ const useStyles = makeStyles((theme: Theme) => ({
 }));
 
 export default function CreateCoursePage() {
+  const { enqueueSnackbar } = useSnackbar();
   const classes = useStyles();
+
+  const courseHandler = async () => {
+    const { success, message } = await post("/courses", {
+      courseName: courseName,
+      instructor: instructor,
+      description: description,
+    });
+    if (!success) {
+      enqueueSnackbar(
+        message ?? `Couldn't add "${courseName}" to your courses!`
+      );
+    } else {
+      enqueueSnackbar(message ?? `Added ${courseName} to your courses!`);
+    }
+  };
+
+  const [courseName, setCourseName] = useState("");
+  const [instructor, setInstructor] = useState("");
+  const [description, setDescription] = useState("");
+
+  const handleCourseNameChange = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setCourseName(event.target.value);
+  };
+
+  const handleInstructorChange = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setInstructor(event.target.value);
+  };
+
+  const handleDescriptionChange = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setDescription(event.target.value);
+  };
 
   return (
     <div className={classes.root}>
@@ -29,6 +70,7 @@ export default function CreateCoursePage() {
             variant="contained"
             color="primary"
             className={classes.button}
+            onClick={courseHandler}
           >
             Create
           </Button>
@@ -41,7 +83,14 @@ export default function CreateCoursePage() {
           </Button>
         </div>
       </div>
-      <Typography variant="h4">form section</Typography>
+      <CreateCourseForm
+        courseName={courseName}
+        instructor={instructor}
+        description={description}
+        changeCourseName={handleCourseNameChange}
+        changeInstructor={handleInstructorChange}
+        changeDescription={handleDescriptionChange}
+      />
     </div>
   );
 }
