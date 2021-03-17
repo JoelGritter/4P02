e05b2ed00d0ded0e5f074333649c1c26f806517b
JoelGritter <joel@gritter.ca>
2021-03-17 08:22:45 -0400

    Change course page title

--- frontend/src/pages/CoursePage.tsx
+++ frontend/src/pages/CoursePage.tsx
@@ -3,16 +3,16 @@ import React from 'react';
 import { Link, useParams } from 'react-router-dom';
 import useGet from '../api/data/use-get';
 import { Helmet } from 'react-helmet-async';
+import Course from '../api/data/models/course.model';
 
 // TODO: Add redirect functionality/error handling if course does not exist
 export default function CoursesPage() {
   const { id } = useParams() as any;
-  const { data: course, loading, failed } = useGet(`/course/${id}`);
-  const courseName = "Kindeep pls help";
+  const { data: course, loading, failed } = useGet<Course>(`/course/${id}`);
   return (
     <>
       <Helmet>
-        <title>uAssign - {courseName}</title>
+        <title>uAssign - {course?.name || 'Course Loading...'}</title>
       </Helmet>
       <div>
         {course && (
