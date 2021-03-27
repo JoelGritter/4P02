38f17d7bde194d3bbe37b41c9b2f39adbe35c614
MzFattal <mzfattal@hotmail.com>
2021-03-27 17:50:26 -0400

    completed course page, displayed assignments

---
+++ frontend/src/components/AssignmentCard.tsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+import Assignment from '../api/data/models/assignment.model';
+import moment from 'moment';
+
+const useStyles = makeStyles({
+  root: {
+    margin: 5,
+  },
+  contentContainer: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    flexDirection: 'row',
+  },
+});
+
+interface AssignmentCardProps {
+  assignment: Assignment;
+}
+
+const AssignmentCard: React.FC<AssignmentCardProps> = ({ assignment }) => {
+  const classes = useStyles();
+
+  return (
+    <Card className={classes.root}>
+      <Link
+        to={`/assignment/${assignment.courseID}`}
+        style={{ textDecoration: 'none' }}
+      >
+        <CardActionArea>
+          <CardContent className={classes.contentContainer}>
+            <Typography
+              gutterBottom
+              variant="h5"
+              color="primary"
+              component="h5"
+            >
+              {assignment.name}
+            </Typography>
+            <Typography variant="body1" color="textSecondary" component="h6">
+              {/* {assignment?.dueDate} */}
+              {moment(assignment?.dueDate).format('DD-MM-YYYY')}
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+      </Link>
+    </Card>
+  );
+};
+
+export default AssignmentCard;
--- frontend/src/pages/CoursePage.tsx
+++ frontend/src/pages/CoursePage.tsx
@@ -4,43 +4,98 @@ import { Link, useParams } from 'react-router-dom';
 import useGet from '../api/data/use-get';
 import { Helmet } from 'react-helmet-async';
 import Course from '../api/data/models/course.model';
+import Assignment from '../api/data/models/assignment.model';
 import RequestStatus from '../components/RequestStatus';
+import { makeStyles, Theme } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+import AssignmentCard from '../components/AssignmentCard';
+// import Assignment from '../api/data/models/assignment.model';
+
+const useStyles = makeStyles((theme: Theme) => ({
+  root: {
+    margin: theme.spacing(1),
+  },
+  header: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    margin: theme.spacing(1),
+  },
+  assignHeader: {
+    margin: theme.spacing(1),
+  },
+  subHeader: {
+    color: 'grey',
+    margin: theme.spacing(1),
+    marginTop: theme.spacing(2),
+  },
+  assignmentContainer: {},
+  createCourseButton: {
+    marginLeft: 5,
+    height: 75,
+  },
+}));
 
 // TODO: Add redirect functionality/error handling if course does not exist
 export default function CoursesPage() {
   const { id } = useParams() as any;
-  const { data: course, loading, failed } = useGet<Course>(`/course/${id}`);
+  const {
+    data: course,
+    loading: loadingCourse,
+    failed: failedCourse,
+  } = useGet<Course>(`/course/${id}`);
+  const {
+    data: assignment,
+    loading: loadingAssignments,
+    failed: failedAssignments,
+  } = useGet<Assignment | any>(`/assign/course/${id}`);
+  const classes = useStyles();
+
   return (
     <>
       <Helmet>
         <title>uAssign - {course?.name || 'Course Loading...'}</title>
       </Helmet>
-      <div>
-        {course && (
+      <div className={classes.root}>
+        {assignment && course && (
           <>
-            <pre>{JSON.stringify(course, null, 2)}</pre>
-            <Button
-              component={Link}
-              to={`/courses/${id}/edit`}
-              variant="contained"
-              color="primary"
-            >
-              Edit Course
-            </Button>
-            <Button
-              style={{ marginLeft: 5 }}
-              component={Link}
-              to={`/courses/${id}/assignments/create`}
-              variant="contained"
-              color="primary"
-            >
-              Create Assignment
-            </Button>
+            <div className={classes.header}>
+              <Typography variant="h4">{course.name}</Typography>
+              <Button
+                component={Link}
+                to={`/courses/${id}/edit`}
+                variant="contained"
+                color="primary"
+              >
+                Edit Course
+              </Button>
+            </div>
+            <Typography className={classes.subHeader}>
+              {course.description}
+            </Typography>
+            <div className={classes.assignmentContainer}>
+              <Typography variant="h5" className={classes.assignHeader}>
+                Assignments
+              </Typography>
+              {assignment.map((assignments: Assignment) => {
+                return <AssignmentCard assignment={assignments} />;
+              })}
+              <Button
+                className={classes.createCourseButton}
+                component={Link}
+                fullWidth
+                to={`/courses/${id}/assignments/create`}
+                variant="contained"
+                color="primary"
+              >
+                Create Assignment
+              </Button>
+            </div>
           </>
         )}
         <RequestStatus
-          failed={failed}
-          loading={loading}
+          failed={failedCourse || failedAssignments}
+          loading={loadingCourse || loadingAssignments}
           failedMessage="Could not load course!"
         />
       </div>
