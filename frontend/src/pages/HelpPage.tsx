04f1d2aee2c6410bffe9906023216fa0696c3bed
AdityaRajyaguru <adirajyaguru@gmail.com>
2021-04-27 19:31:05 -0400

    Removed unusued box import from material UI

--- frontend/src/pages/HelpPage.tsx
+++ frontend/src/pages/HelpPage.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { createStyles, Box } from '@material-ui/core';
+import { createStyles } from '@material-ui/core';
 import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
 import { makeStyles, Theme } from '@material-ui/core/styles';
